97d7d6dd592887d12937a457cf29c74dc90dc1a9
Juan Gauna <95149753+jugauna@users.noreply.github.com>
2023-06-10 19:00:52 -0300

    Python_FastAPI

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "Hola FastAPI"
+
+# Url local: http://127.0.0.1:8000/url
+
+
+@app.get("/url")
+async def url():
+    return {"url": "https://mouredev.com/python"}
+
+
+
+# Inicia el server: uvicorn main:app --reload
+# Detener el server: CTRL+C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
