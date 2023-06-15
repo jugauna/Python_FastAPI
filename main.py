dca2c6e624db011ed36c06e9cb1f164a805f246f
Juan Gauna <95149753+jugauna@users.noreply.github.com>
2023-06-14 21:21:47 -0300

    Python_FastAPI4

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from routers import products, users, basic_auth_users, jwt_auth_users, users_db
+from fastapi.staticfiles import StaticFiles
+
+
 
 app = FastAPI()
+
+app.include_router(products.router)
+app.include_router(users.router)
+app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
+app.include_router(users_db.router)
 
 @app.get("/")
 async def root():
@@ -13,7 +23,7 @@ async def root():
 async def url():
     return {"url": "https://mouredev.com/python"}
 
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Inicia el server: uvicorn main:app --reload
 # Detener el server: CTRL+C
