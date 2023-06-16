5bca9fe25d6509a3cde594f5ba9f700025583891
Juan Gauna <95149753+jugauna@users.noreply.github.com>
2023-06-15 23:28:32 -0300

    Python_FastAPI5

--- db/client.py
+++ db/client.py
@@ -13,13 +13,13 @@ from pymongo import MongoClient
 # Descomentar el db_client local o remoto correspondiente
 
 # Base de datos local MongoDB
-db_client = MongoClient().local
+#db_client = MongoClient().local
 
 # Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=25470
 
 # Base de datos remota MongoDB Atlas (https://mongodb.com)
-# db_client = MongoClient(
-#     "mongodb+srv://<user>:<password>@<url>/?retryWrites=true&w=majority").test
+db_client = MongoClient(
+     "mongodb+srv://test:test@cluster0.6ghiuoa.mongodb.net/?retryWrites=true&w=majority").test
 
 # Despliegue API en la nube:
 # Deta - https://www.deta.sh/
